08f261e26422fd55f7dd467fe1e278a888da9d66
RedVeil <leon.niesler@gmail.com>
2021-11-08 17:35:22 +0100

    created landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,17 @@
 import { Web3Provider } from "@ethersproject/providers";
 import { useWeb3React } from "@web3-react/core";
-import Navbar from "components/NavBar";
-import { connectors } from "context/Web3/connectors";
 import { ContractContext } from "context/Web3/contracts";
 import { useRouter } from "next/router";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 
 const IndexPage = () => {
   const router = useRouter();
   const context = useWeb3React<Web3Provider>();
   const { contract } = useContext(ContractContext);
   const { library, account, activate, active } = context;
+  const endDate = 1638172800000; //Nov 29, 08.00.00 UTC
+  const [countdown, setCountdown] = useState<number[]>([]);
+  const [countdownActive, disableCountdown] = useState<boolean>(true);
 
   useEffect(() => {
     if (typeof window !== "undefined" && window.location.pathname !== "/") {
@@ -19,43 +20,76 @@ const IndexPage = () => {
   }, [router.pathname]);
 
   useEffect(() => {
-    if (!account || !contract) {
-      return;
+    if (countdownActive) {
+      setInterval(function () {
+        const now = new Date().getTime();
+
+        const distance = endDate - now;
+        if (distance < 0) {
+          disableCountdown(false);
+          setCountdown([0, 0, 0, 0]);
+        }
+
+        // Time calculations for days, hours, minutes and seconds
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor(
+          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        setCountdown([days, hours, minutes, seconds]);
+      }, 1000);
     }
-    if (true) {
-      router.push("/claim");
-    } else {
-      router.push("/error");
-    }
-  }, [account]);
+  }, []);
 
   return (
     <>
       {/* Desktop + Large Desktop */}
       <div className="hidden xl:flex flex-col w-full h-screen bg-primaryLight">
-        <Navbar />
-        <div className="w-9/12 mx-auto mt-16 2xl:mt-40">
-          <h1 className="text-8xl font-light w-1/2">Claim your Airdrop Now!</h1>
-          <p className="mt-8 text-2xl 2xl:text-3xl w-5/12 ">
-            Awesome that you have participated in our Airdrop. Now is the time
-            to claim your eligible token by connecting your{" "}
-            <span className="font-medium">Metamask wallet</span>.
-          </p>
+        <div className="mx-auto mt-8">
+          <img src="/textLogo.png" className="h-14 w-64" />
+        </div>
+        <div className="w-9/12 mx-auto z-20 mt-40">
+          <div className="flex flex-row justify-evenly">
+            <div className="text-center">
+              <h1 className="font-bold text-7xl leading-snug">
+                {countdown[0]}
+              </h1>
+              <p className="text-3xl font-landing text-gray-800">Days</p>
+            </div>
+            <div className="text-center">
+              <h1 className="font-bold text-7xl leading-snug">
+                {countdown[1]}
+              </h1>
+              <p className="text-3xl font-landing text-gray-800">Hours</p>
+            </div>
+            <div className="text-center">
+              <h1 className="font-bold text-7xl leading-snug">
+                {countdown[2]}
+              </h1>
+              <p className="text-3xl font-landing text-gray-800">Minutes</p>
+            </div>
+            <div className="text-center">
+              <h1 className="font-bold text-7xl leading-snug">
+                {countdown[3]}
+              </h1>
+              <p className="text-3xl font-landing text-gray-800">Seconds</p>
+            </div>
+          </div>
+          <h3 className="font-light text-gray-800 text-4xl pt-12 text-center">
+            Coming soon...
+          </h3>
         </div>
         <img
-          src="/images/Mint blue cat hands up.svg"
+          src="/images/mobileBlueCat.svg"
           alt="catHandsUp"
-          className="absolute right-40 bottom-60 2xl:bottom-80 z-10 2xl:w-5/12"
-        />
-        <img
-          src="/images/Mint blue cat with umbrella.svg"
-          alt="catUmbrella"
-          className="absolute bottom-20 right-60 2xl:right-112 z-10 2xl:w-4/12"
+          className="absolute right-40 bottom-30 w-3/12 2xl:bottom-80 z-10 2xl:w-3/12"
         />
         <img
           src="/images/Yellow_cat.png"
           alt="catYellow"
-          className="absolute bottom-20 2xl:bottom-80 left-40 z-10 2xl:w-4/12"
+          className="absolute bottom-28 2xl:bottom-72 left-40 z-10 2xl:w-5/12"
         />
         <img
           src="/images/Background_ hill.svg"
@@ -65,33 +99,77 @@ const IndexPage = () => {
       </div>
       {/* Mobile + Tablet */}
       <div className="w-full h-screen xl:hidden bg-primaryLight">
-        <Navbar />
-        <div className="relative w-10/12 text-center mx-auto mt-12 md:mt-40 lg:mt-56 z-20">
-          <h1 className="text-5xl font-light">Claim your Airdrop Now!</h1>
-          <p className="mt-4 text-2xl font-light">
-            Awesome that you have participated in our Airdrop. Now is the time
-            to claim your eligible token by connecting your{" "}
-            <span className="font-medium">Metamask wallet</span>.
-          </p>
-          <button
-            className="relative w-48 py-3 px-3 mt-4 z-40 mx-auto flex flex-row items-center justify-center rounded-xl cursor-pointer bg-blue-600 hover:bg-blue-700"
-            onClick={() => activate(connectors.Injected)}
-          >
-            <p className="text-xl font-medium text-white">Connect Wallet</p>
-          </button>
+        <div className="w-full flex flex-row justify-center pt-12">
+          <img src="/textLogo.png" className="h-14 w-64" />
         </div>
-        <img
-          src="/images/mobileBlueCat.svg"
-          alt="catHandsUp"
-          className="absolute right-0 bottom-5 md:bottom-20 md:right-5 lg:right-20 z-10 md:w-4/12 lg:w-3/12"
-        />
+        <div className="w-9/12 mx-auto z-20 mt-10 md:mt-40">
+          <div className="flex flex-col md:hidden">
+            <div className="flex flex-row justify-evenly">
+              <div className="text-center">
+                <h1 className="font-bold text-7xl leading-snug">
+                  {countdown[0]}
+                </h1>
+                <p className="text-3xl font-landing text-gray-800">Days</p>
+              </div>
+              <div className="text-center">
+                <h1 className="font-bold text-7xl leading-snug">
+                  {countdown[1]}
+                </h1>
+                <p className="text-3xl font-landing text-gray-800">Hours</p>
+              </div>
+            </div>
+            <div className="flex flex-row justify-evenly">
+              <div className="text-center">
+                <h1 className="font-bold text-7xl leading-snug">
+                  {countdown[2]}
+                </h1>
+                <p className="text-3xl font-landing text-gray-800">Minutes</p>
+              </div>
+              <div className="text-center">
+                <h1 className="font-bold text-7xl leading-snug">
+                  {countdown[3]}
+                </h1>
+                <p className="text-3xl font-landing text-gray-800">Seconds</p>
+              </div>
+            </div>
+          </div>
+          <div className="hidden md:flex flex-row justify-evenly">
+            <div className="text-center">
+              <h1 className="font-bold text-7xl leading-snug">
+                {countdown[0]}
+              </h1>
+              <p className="text-3xl font-landing text-gray-800">Days</p>
+            </div>
+            <div className="text-center">
+              <h1 className="font-bold text-7xl leading-snug">
+                {countdown[1]}
+              </h1>
+              <p className="text-3xl font-landing text-gray-800">Hours</p>
+            </div>
+            <div className="text-center">
+              <h1 className="font-bold text-7xl leading-snug">
+                {countdown[2]}
+              </h1>
+              <p className="text-3xl font-landing text-gray-800">Minutes</p>
+            </div>
+            <div className="text-center">
+              <h1 className="font-bold text-7xl leading-snug">
+                {countdown[3]}
+              </h1>
+              <p className="text-3xl font-landing text-gray-800">Seconds</p>
+            </div>
+          </div>
+          <h3 className="font-light text-gray-800 text-4xl pt-8 text-center">
+            Coming soon...
+          </h3>
+        </div>
         <img
           src="/images/Yellow_cat.png"
           alt="catYellow"
-          className="absolute bottom-16 md:bottom-36 left-0 lg:left-18 w-8/12 lg:w-6/12 z-10"
+          className="absolute bottom-6 lg:left-20 lg:w-6/12 z-10 "
         />
         <img
-          src="/images/mobileBg.svg"
+          src="/images/Background_ hill.svg"
           alt="bgHill"
           className="absolute bottom-0 w-full"
         />
