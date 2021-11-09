faf7f8e7f32595d3ae2c484e17668f3f421d9f31
RedVeil <leon.niesler@gmail.com>
2021-11-09 18:07:38 +0100

    desktop design

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -101,46 +101,69 @@ const IndexPage = () => {
     <>
       {/* Desktop + Large Desktop */}
       <div className="hidden xl:flex flex-col w-full h-screen overflow-hidden bg-primaryLight">
-        <div className="w-full flex flex-row justify-center pt-8">
-          <img src="/textLogo.png" className="h-10 w-54" />
+        <div className="mx-auto mt-8">
+          <img src="/textLogo.png" className="h-14 w-64" />
         </div>
-        <div className="w-10/12 mx-auto z-40 mt-14 md:mt-40">
-          <h3 className="w-full relative mx-auto font-light text-gray-800 text-4xl text-center z-20">
+        <div className="w-5/12 mx-auto z-20 mt-24">
+          <h3 className="w-full relative mx-auto font-light text-gray-800 text-5xl text-center z-20">
             Airdrop registration open until
           </h3>
-          <div className="flex flex-row justify-evenly mt-6">
+          <div className="flex flex-row justify-evenly mt-8">
             <div className="text-center">
-              <h1 className="font-medium text-4xl leading-snug">
+              <h1 className="font-bold text-7xl leading-snug">
                 {countdown[0]}
               </h1>
-              <p className="text-2xl font-light text-gray-800">Hours</p>
+              <p className="text-3xl font-landing text-gray-800">Hours</p>
             </div>
             <div className="text-center">
-              <h1 className="font-medium text-4xl leading-snug">
+              <h1 className="font-bold text-7xl leading-snug">
                 {countdown[1]}
               </h1>
-              <p className="text-2xl font-light text-gray-800">Minutes</p>
+              <p className="text-3xl font-landing text-gray-800">Minutes</p>
             </div>
             <div className="text-center">
-              <h1 className="font-medium text-4xl leading-snug">
+              <h1 className="font-bold text-7xl leading-snug">
                 {countdown[2]}
               </h1>
-              <p className="text-2xl font-light text-gray-800">Seconds</p>
+              <p className="text-3xl font-landing text-gray-800">Seconds</p>
             </div>
           </div>
           {isDuplicate ? (
-            <h3 className="text-center text-2xl font-light pb-2 pt-18">
-              Code was already used
-            </h3>
-          ) : (
             <>
               <h3 className="text-center text-2xl font-light pb-2 pt-18">
+                Code was already used...
+              </h3>
+              <p className="text-xl 2xl:text-5xl font-light text-center mt-4">
+                Be sure to join our Liquidity Event on{" "}
+                <span className="font-medium">Nov. 29</span>. Join our community
+                on
+                <a
+                  className="font-medium cursor-pointer ml-2"
+                  href="https://discord.gg/RN4VGqPDwX"
+                  target="_blank"
+                >
+                  Discord
+                </a>{" "}
+                or follow us on
+                <a
+                  className="font-medium cursor-pointer ml-1"
+                  href="https://twitter.com/popcorn_DAO"
+                  target="_blank"
+                >
+                  Twitter
+                </a>{" "}
+                for more information!
+              </p>
+            </>
+          ) : (
+            <>
+              <h3 className="text-center text-3xl font-light pb-4 pt-18">
                 {step === Step.Network
-                  ? "Choose Network"
+                  ? "Choose a Network"
                   : "Register for Airdrop"}
               </h3>
               {step === Step.Network ? (
-                <div className="mx-auto w-full flex flex-row items-center">
+                <div className="mx-auto w-6/12 flex flex-row items-center">
                   <button
                     className="relative w-full py-3 px-3 mr-2 z-20 flex flex-row items-center justify-center rounded-lg cursor-pointer bg-blue-600 hover:bg-blue-700"
                     onClick={() => {
@@ -161,12 +184,12 @@ const IndexPage = () => {
                   </button>
                 </div>
               ) : (
-                <>
+                <div className="w-8/12 mx-auto">
                   <div className="relative z-20 mx-auto shadow-lg bg-white rounded-xl py-2 px-2 w-full flex flex-row items-center justify-between">
                     <input
                       type="text"
                       name="address"
-                      className="w-10/12 p-2 text-base mx-2 text-gray-900"
+                      className="w-10/12 p-2 border-none text-base mx-2 text-gray-900"
                       placeholder="ethereum address"
                       value={address}
                       onChange={(e) => setAddress(e.target.value)}
@@ -182,11 +205,11 @@ const IndexPage = () => {
                       />
                     </div>
                   </div>
-                  <h3 className="text-center text-lg font-light pt-3 pb-2">
+                  <h3 className="text-center text-2xl font-light pt-5 pb-4">
                     Or
                   </h3>
                   <button
-                    className="relative w-full py-3 px-3 z-20 flex flex-row items-center justify-center rounded-lg cursor-pointer bg-blue-600 hover:bg-blue-700"
+                    className="mx-auto relative w-8/12 py-3 px-3 z-20 flex flex-row items-center justify-center rounded-lg cursor-pointer bg-blue-600 hover:bg-blue-700"
                     onClick={() => {
                       activate(connectors.Injected);
                       setAddress(account);
@@ -197,15 +220,20 @@ const IndexPage = () => {
                       Connect Wallet
                     </p>
                   </button>
-                </>
+                </div>
               )}
             </>
           )}
         </div>
         <img
+          src="/images/mobileBlueCat.svg"
+          alt="catHandsUp"
+          className="absolute right-40 bottom-30 w-3/12 2xl:bottom-80 z-10 2xl:w-3/12"
+        />
+        <img
           src="/images/Yellow_cat.png"
           alt="catYellow"
-          className="absolute bottom-6 lg:left-20 lg:w-6/12 z-10 "
+          className="absolute bottom-28 2xl:bottom-72 left-40 z-10 2xl:w-5/12"
         />
         <img
           src="/images/Background_ hill.svg"
@@ -274,7 +302,7 @@ const IndexPage = () => {
             <>
               <h3 className="text-center text-2xl font-light pb-2 pt-18">
                 {step === Step.Network
-                  ? "Choose Network"
+                  ? "Choose a Network"
                   : "Register for Airdrop"}
               </h3>
               {step === Step.Network ? (
